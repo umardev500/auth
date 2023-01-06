0242058baacfe92a224bfbe2e70f3bbb229bb946
umar <umar.dev500@gmail.com>
2023-01-06 21:20:10 +0700

    make variable env to descrptive

--- middleware/limiter.go
+++ middleware/limiter.go
@@ -19,14 +19,15 @@ func RateLimiter(storage fiber.Storage) (f func(ctx *fiber.Ctx) error) {
 			return ctx.Status(http.StatusBadRequest).JSON(http.StatusText(http.StatusBadRequest))
 		}
 
-		expiredTime, _ := strconv.Atoi(os.Getenv("EXPIRED_HOURS"))
+		maxReq, _ := strconv.Atoi(os.Getenv("LOGIN_MAX_REQ"))
+		expiredTime, _ := strconv.Atoi(os.Getenv("LOGIN_EXPIRED_TIME"))
 
 		return limiter.New(limiter.Config{
 			Next: func(c *fiber.Ctx) bool {
 				return c.IP() == "500.0.0.1"
 			},
-			Max:        1,
-			Expiration: time.Duration(expiredTime) * time.Minute,
+			Max:        maxReq,
+			Expiration: time.Duration(expiredTime) * time.Second,
 			KeyGenerator: func(c *fiber.Ctx) string {
 				return c.Get("userid")
 			},
