aead0099972ceef374e86265dfbd43995fc890a6
umar <umar.dev500@gmail.com>
2023-01-08 23:16:32 +0700

    add check userid header

--- middleware/limiter.go
+++ middleware/limiter.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"errors"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -11,27 +9,27 @@ import (
 )
 
 func RateLimiter(storage fiber.Storage) (f func(ctx *fiber.Ctx) error) {
-
-	f = limiter.New(limiter.Config{
-		Next: func(c *fiber.Ctx) bool {
-			return c.IP() == "500.0.0.1"
-		},
-		Max:        1,
-		Expiration: 1 * time.Minute,
-		KeyGenerator: func(c *fiber.Ctx) string {
-			return c.Get("userid")
-		},
-		LimitReached: func(c *fiber.Ctx) error {
-			return c.JSON(http.StatusText(http.StatusTooManyRequests))
-		},
-		Storage: storage,
-	})
-
 	f = func(ctx *fiber.Ctx) error {
-		userId := ctx.Get("userid")
-		fmt.Println(userId)
 
-		return errors.New(userId)
+		userId := ctx.Get("userid")
+		if userId == "" {
+			return ctx.Status(http.StatusBadRequest).JSON(http.StatusText(http.StatusBadRequest))
+		}
+
+		return limiter.New(limiter.Config{
+			Next: func(c *fiber.Ctx) bool {
+				return c.IP() == "500.0.0.1"
+			},
+			Max:        1,
+			Expiration: 1 * time.Minute,
+			KeyGenerator: func(c *fiber.Ctx) string {
+				return c.Get("userid")
+			},
+			LimitReached: func(c *fiber.Ctx) error {
+				return c.JSON(http.StatusText(http.StatusTooManyRequests))
+			},
+			Storage: storage,
+		})(ctx)
 	}
 
 	return
