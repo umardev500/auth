c43c994fe4c2eb63c3d072e6166506f1d61cce8a
umar <umar.dev500@gmail.com>
2023-01-08 10:00:43 +0700

    create token

--- app/delivery/admin_delivery.go
+++ app/delivery/admin_delivery.go
@@ -1,12 +1,19 @@
 package delivery
 
 import (
+	"auth/domain"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type adminDelivery struct{}
+
+var validate = validator.New()
 
 func NewAdminDelivery(router fiber.Router) {
 	handler := &adminDelivery{}
@@ -14,8 +21,48 @@ func NewAdminDelivery(router fiber.Router) {
 	router.Post("/admin", handler.Login)
 }
 
+func (a *adminDelivery) sendLoginResponse(ctx *fiber.Ctx, statusCode int, message string, token *string) error {
+	var tokenValue string
+	if token != nil {
+		tokenValue = *token
+	}
+
+	response := domain.LoginResponse{
+		StatusCode: statusCode,
+		Message:    message,
+		Token:      tokenValue,
+	}
+	return ctx.JSON(response)
+}
+
 func (a *adminDelivery) Login(ctx *fiber.Ctx) error {
-	return ctx.JSON("login")
+	var req domain.LoginRequest
+	if err := ctx.BodyParser(&req); err != nil {
+		response := domain.LoginResponse{
+			StatusCode: http.StatusBadRequest,
+			Message:    err.Error(),
+		}
+		return ctx.JSON(response)
+	}
+
+	if err := validate.Struct(&req); err != nil {
+		return a.sendLoginResponse(ctx, http.StatusBadRequest, err.Error(), nil)
+	}
+
+	secret := os.Getenv("SECRET")
+	claims := jwt.MapClaims{
+		"name": "jack",
+		"exp":  time.Now().UTC().Add(1 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return a.sendLoginResponse(ctx, http.StatusOK, err.Error(), nil)
+	}
+
+	response := a.sendLoginResponse(ctx, http.StatusOK, "Get token", &signedToken)
+	return response
 }
 
 func (a *adminDelivery) Auth(ctx *fiber.Ctx) error {
